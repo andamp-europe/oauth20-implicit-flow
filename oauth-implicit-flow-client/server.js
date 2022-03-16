0b77cd71400a7fada7cd119b576a449d1eb884e7
Madina Basnakajeva <madina.basnakajeva@andamp.io>
2022-03-16 15:31:55 +0100

    Improve code readability and fix Typos

--- oauth-implicit-flow-client/server.js
+++ oauth-implicit-flow-client/server.js
@@ -1,11 +1,11 @@
 const express = require("express");
-const utils = require("./utils");
 const path = require("path");
 const cors = require("cors");
-const port = 8082;
+const utils = require("./utils");
 const router = express();
+const port = 8082;
 
-let token_for_further_processes = "";
+const state = "8njkfds893ksHSD3bd";
 
 router.use(express.static(__dirname + "/public"));
 router.use(
@@ -15,16 +15,13 @@ router.use(
 );
 
 router.get("/auth", async (req, res) => {
-  console.log("auth endpoint called");
   try {
-    res.redirect(utils.request_get_auth_code_url);
+    res.redirect(utils.auth_server_request(state));
   } catch (error) {
     res.sendStatus(500);
     console.error(error.message);
   }
 });
-
-router.get("/redirected", async (req, res) => {});
 
 router.get("/login/oauth/callback", async (req, res) => {
   res.sendFile(path.join(__dirname, "/public/OAuthRedirect.html"));
@@ -33,10 +30,17 @@ router.get("/login/oauth/callback", async (req, res) => {
 router.get("/login/callback", async (req, res) => {
   const queryString = req.query;
   if (!req.query) {
-    res.status(404);
-    res.send("No Access Token provided");
+    return res.status(404).send("No Access Token provided");
   }
-  token_for_further_processes = queryString.access_token;
+
+  if (state !== queryString.state) {
+    return res
+      .status(401)
+      .send(
+        "The state variables did not match. Could not identify response validity."
+      );
+  }
+  let token_for_further_processes = queryString.access_token;
 
   res.sendFile(path.join(__dirname, "/public/Greetings.html"));
 });
