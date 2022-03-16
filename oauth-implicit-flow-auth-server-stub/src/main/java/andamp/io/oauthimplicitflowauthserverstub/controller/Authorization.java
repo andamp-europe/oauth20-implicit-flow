6ae822d181664b746b7a8b12aa40cc4272b2dd45
Madina Basnakajeva <madina.basnakajeva@andamp.io>
2022-03-16 09:44:59 +0100

    Remove unused code, use jwt token with correct values, use better naming

--- oauth-implicit-flow-auth-server-stub/src/main/java/andamp/io/oauthimplicitflowauthserverstub/controller/Authorization.java
+++ oauth-implicit-flow-auth-server-stub/src/main/java/andamp/io/oauthimplicitflowauthserverstub/controller/Authorization.java
@@ -12,11 +12,11 @@ import java.net.URI;
 public class Authorization {
 
 
-    private static String access_token_stub="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkFtZCBBbXAiLCJpYXQiOjE1MTYyMzkwMjJ9.m3TEL66NgvxZIYWVcooglfucTtdhiUMF1trTGuA_hYI";
+    private static final String access_token_stub="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJBdXRoIFNlcnZlciBTdHViIiwibmFtZSI6IiZhbXAiLCJpYXQiOjE1MTYyMzkwMjJ9.F8jDm_DMnpOVcpx5cQY5UUOdAMxrqQGv-fXh_V01MEw";
 
     @GetMapping("/oauth2/authorization")
     @ResponseBody
-    public ModelAndView authorize(@RequestParam String response_type, @RequestParam String client_id, @RequestParam String redirect_uri, @RequestParam String scope, @RequestParam String state){
+    public ModelAndView authorizeWithModelAndView(@RequestParam String response_type, @RequestParam String client_id, @RequestParam String redirect_uri, @RequestParam String scope, @RequestParam String state){
         System.out.println("Received params: response_type:" + response_type + "clientID:  "+client_id+" scope: " + scope +" state: "+ state);
         return new ModelAndView( "redirect:"+redirect_uri+"#state="+state+"&access_token="+access_token_stub+"&token_type=bearer"+
                 "&expires_in=900");
@@ -24,7 +24,7 @@ public class Authorization {
 
     @GetMapping("/oauth2/authorization2")
     @ResponseBody
-    public ResponseEntity<Void> authorize2(@RequestParam String response_type, @RequestParam String client_id, @RequestParam String redirect_uri, @RequestParam String scope, @RequestParam String state){
+    public ResponseEntity<Void> authorizeWithResponseEntity(@RequestParam String response_type, @RequestParam String client_id, @RequestParam String redirect_uri, @RequestParam String scope, @RequestParam String state){
         System.out.println("Received params: response_type:" + response_type + "clientID:  "+client_id+" scope: " + scope +" state: "+ state);
         return ResponseEntity.status(HttpStatus.FOUND).location(URI.create(redirect_uri+"#state="+state+"&access_token="+access_token_stub+"&token_type=bearer"+
                 "&expires_in=900")).build();
